4429bb1f3c6084b793d655aff09231a5f8cf0290
foorcun <msnifoorcun@hotmail.com>
2025-12-17 13:23:14 +0300

    knapsack

--- src/main/java/org/example/knapsack/Client.java
+++ src/main/java/org/example/knapsack/Client.java
@@ -1,16 +1,47 @@
 package org.example.knapsack;
 
+import java.util.Arrays;
+
 public class Client {
 
     private static int knapsack(int W, int[] v, int[] w) {
-       return 0;
+//       return 0;
+        int n  = v.length;
+        int[][] memo = new int[n +1][W+1];
+        for(int i =0;i<n+1;i++){
+            Arrays.fill(memo[i],-1);
+        }
+        return helper(W, v, w,v.length, memo);
+    }
+
+    private static int helper(int W, int[] v, int[] w, int n, int[][]memo){
+       //base case n <=0 || W <= 0, return 0
+        if (n<=0 || W <=0) return 0;
+
+        // memo case
+        if(memo[n][W] != -1 ) return memo[n][W];
+
+
+       // limit case w[n-1] > W, next
+        if(w[n-1] > W){
+          memo[n][W] = helper(W, v, w, n-1, memo);
+          return memo[n][W];
+        }
+
+       // recursive - decision case
+       memo[n][W] =  Math.max(
+               helper(W, v, w, n-1, memo),
+              v[n-1]  + helper(W-w[n-1],v, w, n-1, memo)
+       );
+
+       return memo[n][W];
     }
 
     public static void main(String[] args) {
         int W = 10;
         int[] v = {1, 4, 8, 5};
         int[] w = {3, 3, 5, 6};
-        System.out.println(knapsack(W, w, v)); // 12
+        System.out.println(knapsack(W, v, w)); // 12
 //        Pick:
 //        item with w=3, v=4
 //        item with w=5, v=8
@@ -21,7 +52,7 @@ public class Client {
         W = 7;
         v = new int[]{2, 2, 4, 5, 3};
         w = new int[]{3, 1, 3, 4, 2};
-        System.out.println(knapsack(W, w, v)); // 7
+        System.out.println(knapsack(W, v, w)); // 10
 //        Weight = 4 + 1 + 2 = 7
 //        Value = 5 + 2 + 3 = 10
     }
