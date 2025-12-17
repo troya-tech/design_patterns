6a281fcb69c620364d637db71ccdc52b49f043ab
foorcun <msnifoorcun@hotmail.com>
2025-12-18 00:25:39 +0300

    save

--- src/main/java/org/example/coinchange/Client2.java
+++ src/main/java/org/example/coinchange/Client2.java
@@ -30,18 +30,23 @@ public class Client2 {
 
         // dp[a] = minimum coins needed to make amount 'a'
         int[] dp = new int[amount + 1];
+        // mesela amout = 3 ise bizim 4 tane alan olusturmamiz lazim memoride
+        // cunku sifir da bastavar. int[3], 0, 1, 2. int[4], 0,1,2,3. boylelikle dp[3] icin de deger okumasi yapalir
 
         // Fill with "infinity" (unreachable state)
-        Arrays.fill(dp, amount + 1);
+        // Arrays.fill(dp, amount+1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
 
-        // Base case: 0 coins needed to make amount 0
+        // Base case dp[0] = 0
         dp[0] = 0;
 
         // Build solution bottom-up
         for (int a = 1; a <= amount; a++) {
             for (int c : coins) {
                 if (a - c >= 0) {
+                    // dp[a] suanki deger, 1(secme bedeli)+dp[a-c] para secildikten sonra deger
                     dp[a] = Math.min(dp[a], 1 + dp[a - c]);
+                    // yani dp[a] nin suanki degeri mi daha kucuk yoksa para secmenin bir faydasi oldu mu.
                 }
             }
         }
