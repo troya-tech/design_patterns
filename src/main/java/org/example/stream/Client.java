8aa5ee8bdf243ea9ac3e8f0e5863ceb878b3cc4f
foorcun <msnifoorcun@hotmail.com>
2025-12-17 23:51:07 +0300

    stream

---
+++ src/main/java/org/example/stream/Client.java
@@ -0,0 +1,27 @@
+package org.example.stream;
+
+import java.util.List;
+
+public class Client {
+
+    public static void main(String[] args) {
+        var persons = List.of(
+                new Person("aaa", 11),
+                new Person("bbb", 222)
+                );
+
+        System.out.println("#########");
+        persons.stream().forEach(System.out::println);
+        System.out.println("#########");
+
+
+        List<Person>  minorsPersons = persons.stream()
+                .filter(p -> p.age < 18)
+                .toList();
+        System.out.println(minorsPersons);
+
+    }
+
+    record Person(String name, int age) {
+    }
+}
