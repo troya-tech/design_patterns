338b29343d3e588990b96c19d9641c022b5be31d
foorcun <msnifoorcun@hotmail.com>
2025-12-18 15:23:56 +0300

    save

--- src/main/java/org/example/fibonacci/Client.java
+++ src/main/java/org/example/fibonacci/Client.java
@@ -12,21 +12,7 @@ public class Client {
     }
 
     public static int fib(int n) {
-//        return 0;
-        int[] memo = new int[n+1];
-        Arrays.fill(memo, -1);
-        return helper(n, memo);
+        return 0;
     }
 
-    private static int helper(int n, int[] memo){
-        //base case
-        if(n<=1) return n;
-
-        // memo case
-        if(memo[n] != -1) return memo[n];
-
-        // recursive case
-        memo[n] =  helper(n-1,memo) + helper(n-2,memo);
-        return  memo[n];
-    }
 }
