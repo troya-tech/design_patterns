b560a4be649418dec4f94ad813f338d411c4854b
foorcun <msnifoorcun@hotmail.com>
2025-12-22 22:32:27 +0300

    firstUniqueChar

---
+++ src/main/java/org/example/firstUniqueChar/Client.java
@@ -0,0 +1,13 @@
+package org.example.firstUniqueChar;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        System.out.println(firstUniqueChar("looa"));
+    }
+
+    public static int firstUniqueChar(String s){
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/firstUniqueChar/Client2.java
@@ -0,0 +1,23 @@
+package org.example.firstUniqueChar;
+
+public class Client2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(firstUniqueChar("looa"));
+    }
+
+    public static int firstUniqueChar(String s) {
+        int[] freq = new int[256];
+
+        // count the freq
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i)] == 1) return i;
+        }
+        return -1;
+    }
+}
