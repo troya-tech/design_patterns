f9fa53df77369631f7c6ef20cc2cb45f1843da88
foorcun <msnifoorcun@hotmail.com>
2025-12-27 17:52:00 +0300

    save

---
+++ src/main/java/org/example/leetcode/subsets78/Solution.java
@@ -0,0 +1,41 @@
+package org.example.leetcode.subsets78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+
+        return bt(
+                new ArrayList<>(),
+                new ArrayList<>(),
+                nums,
+                0
+        );
+    }
+
+    private List<List<Integer>> bt(
+            List<List<Integer>> res,
+            List<Integer> temp,
+            int[] cand,
+            int p
+    ) {
+
+        res.add(new ArrayList<>(temp));
+
+
+        for (int i = p; i < cand.length; i++) {
+
+            temp.add(cand[i]);
+            bt(res, temp, cand, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+
+        return res;
+    }
+}
