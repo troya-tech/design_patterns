a5b08ce99516718dbe66e21cff5f7947e284c885
foorcun <msnifoorcun@hotmail.com>
2025-12-27 18:01:46 +0300

    save

---
+++ src/main/java/com/leetcode/binarysearch/Problem.java
@@ -0,0 +1,8 @@
+package com.leetcode.binarysearch;
+
+// https://leetcode.com/problems/binary-search/description/
+public class Problem {
+    public int search(int[] nums, int target) {
+        return 0;
+    }
+}
