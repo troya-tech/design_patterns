a7ba051327eed7312427b049eaf372082361e292
foorcun <msnifoorcun@hotmail.com>
2025-12-27 15:50:48 +0300

    bridge

---
+++ src/main/java/org/example/bridge2/Remote.java
@@ -0,0 +1,14 @@
+package org.example.bridge2;
+
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
