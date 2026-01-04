5f67081bca5fb342d8d8b7cb132d353a21fc61d1
foorcun <msnifoorcun@hotmail.com>
2026-01-04 15:30:21 +0300

    save

--- src/main/java/com/leetcode/twosum/Problem.java
+++ src/main/java/com/leetcode/twosum/Problem.java
@@ -1,7 +1,24 @@
 package com.leetcode.twosum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Problem {
     public int[] twoSum(int[] nums, int target) {
-        return null;
+//        return null;
+
+        Map<Integer,Integer> hm = new HashMap<>();
+        int remain;
+
+        for(int i =0 ;i<nums.length;i++){
+            remain = target - nums[i];
+
+            if(hm.containsKey(remain)) return new int[]{i, remain};
+
+            hm.put(nums[i], i );
+
+        }
+       throw new RuntimeException();
     }
+
 }
